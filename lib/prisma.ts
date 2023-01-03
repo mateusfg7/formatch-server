cd7ac2741743805319c7fd1ab81daf65f5371780
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2023-01-03 10:22:30 -0300

    create prisma client

---
+++ lib/prisma.ts
@@ -0,0 +1,15 @@
+import { PrismaClient } from '@prisma/client'
+
+declare global {
+  var prismaClient: PrismaClient | undefined // This must be a `var` and not a `let / const`
+}
+
+const prismaClient =
+  global.prismaClient ||
+  new PrismaClient({
+    log: ['query', 'error'],
+  })
+
+if (process.env.NODE_ENV === 'development') global.prismaClient = prismaClient
+
+export default prismaClient
