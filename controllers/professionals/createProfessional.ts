9f6825ef83ba5670f2eede7381a81e59c8e9c075
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2023-01-04 19:44:38 -0300

    create CRUD controllers for professionals
    * create
    * delete
    * find
    * update

---
+++ controllers/professionals/createProfessional.ts
@@ -0,0 +1,140 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+import { z } from 'zod'
+import { File } from 'formidable'
+
+import { prismaClient } from '../../lib/prisma'
+import { uploadFileToGCS } from '../../utils/uploadFileToGcs'
+import { form } from '../../utils/form'
+import { generateUid } from '../../utils/uid'
+
+export async function createProfessional(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  try {
+    form.parse(req, async (err, rawFields, files) => {
+      if (err) {
+        console.error(err)
+        return res
+          .status(500)
+          .json({ error: err, message: 'Internal server error' })
+      }
+
+      const fieldsSchema = z.object({
+        user_owner_id: z.string(),
+        name: z.string(),
+        email: z.string().email().optional(),
+        phone: z.string().optional(),
+        state_uf: z.string(),
+        city: z.string(),
+        biography: z.string(),
+        whatsapp: z.string().optional(),
+        instagram: z.string().optional(),
+        services: z.string(),
+      })
+      const parsedFields = fieldsSchema.safeParse(rawFields)
+
+      if (!parsedFields.success)
+        return res.status(400).json({ error: parsedFields.error.issues })
+
+      const {
+        user_owner_id,
+        name,
+        email,
+        phone,
+        state_uf,
+        city,
+        biography,
+        whatsapp,
+        instagram,
+        services,
+      } = {
+        ...parsedFields.data,
+        services: (rawFields.services as string).split(','),
+      }
+
+      if (!email && !phone && !whatsapp && !instagram) {
+        return res.status(400).json({
+          message:
+            'You need to have at least one way of contact: email, phone, whatsapp or instagram.',
+        })
+      }
+
+      const file = files.profile_picture_file as File
+
+      if (!file) {
+        return res
+          .status(400)
+          .json({ message: 'File was not sent or parameter is invalid.' })
+      }
+
+      try {
+        const registeredProfessional = await prismaClient.professional.findMany(
+          {
+            where: {
+              user_owner_id,
+            },
+          }
+        )
+
+        if (registeredProfessional.length > 0)
+          return res
+            .status(409)
+            .json({ message: 'User already registered as a professional.' })
+      } catch (err) {
+        console.error(err)
+        return res
+          .status(500)
+          .json({ error: err, message: 'Internal server error' })
+      }
+
+      const parsedProfessionalName = name.toLowerCase().replaceAll(' ', '-')
+      const parsedFileType = file.mimetype?.split('/')[1]
+      const newFileName = `${generateUid()}_${parsedProfessionalName}.${parsedFileType}`
+
+      const localFilePath = file.filepath
+      const gcsFilePath = `assets/profile-picture/${newFileName}`
+
+      try {
+        const fileUrlOnGCS = await uploadFileToGCS(localFilePath, gcsFilePath)
+        const professional = await prismaClient.professional.create({
+          data: {
+            name,
+            email,
+            phone,
+            state_uf,
+            city,
+            biography,
+            whatsapp,
+            instagram,
+            profile_picture_url: fileUrlOnGCS,
+            profile_picture_gcs_path: gcsFilePath,
+            User: {
+              connect: {
+                id: user_owner_id,
+              },
+            },
+            services: {
+              connectOrCreate: services.map((service) => ({
+                where: { service_name: service.trim() },
+                create: { service_name: service.trim() },
+              })),
+            },
+          },
+          include: {
+            services: true,
+          },
+        })
+        return res.status(201).json({ professional })
+      } catch (err) {
+        console.error(err)
+        return res
+          .status(500)
+          .json({ error: err, message: 'Internal server error' })
+      }
+    })
+  } catch (e) {
+    console.error(e)
+    return res.status(500).json({ error: e, message: 'Internal server error' })
+  }
+}
---
+++ controllers/professionals/deleteProfessional.ts
@@ -0,0 +1,53 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+import { z } from 'zod'
+
+import { prismaClient } from '../../lib/prisma'
+import { deleteFileFromGcs } from '../../utils/deleteFileFromGcs'
+
+export async function deleteProfessional(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const { body } = req
+
+  const professionalDataSchema = z.object({
+    professional_id: z.string(),
+  })
+  const parsedProfessionalData = professionalDataSchema.safeParse(body)
+
+  if (!parsedProfessionalData.success)
+    return res.status(400).json({ error: parsedProfessionalData.error.issues })
+
+  const { professional_id } = parsedProfessionalData.data
+
+  try {
+    const professional = await prismaClient.professional.findMany({
+      where: {
+        id: professional_id,
+      },
+    })
+
+    if (professional.length === 0)
+      return res.status(404).json({ message: 'Professional not found.' })
+
+    const deleteFromGcsResponse = await deleteFileFromGcs(
+      professional[0].profile_picture_gcs_path
+    )
+    if (deleteFromGcsResponse.statusCode !== 204)
+      return res
+        .status(500)
+        .json({ message: 'Error while deleting professional.' })
+
+    await prismaClient.professional.deleteMany({
+      where: {
+        id: professional_id,
+      },
+    })
+    res.status(204).end()
+  } catch (err) {
+    console.error(err)
+    return res
+      .status(500)
+      .json({ error: err, message: 'Internal server error' })
+  }
+}
