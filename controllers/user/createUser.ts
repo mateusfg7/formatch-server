d901cd78378fe5c679fe800c3f3287a00816e108
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2023-01-05 22:23:39 -0300

    add `createUser()` controller

---
+++ controllers/user/createUser.ts
@@ -0,0 +1,72 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+import { z } from 'zod'
+import jwt from 'jsonwebtoken'
+
+import { prismaClient } from '../../lib/prisma'
+
+export async function createUser(req: NextApiRequest, res: NextApiResponse) {
+  const requestBodySchema = z.object({
+    access_token: z.string(),
+  })
+
+  const parsedRequestBody = requestBodySchema.safeParse(req.body)
+
+  if (!parsedRequestBody.success)
+    return res.status(400).json({ error: parsedRequestBody.error.issues })
+
+  const { access_token } = parsedRequestBody.data
+
+  const googleUserData = await fetch(
+    'https://www.googleapis.com/oauth2/v2/userinfo',
+    {
+      method: 'GET',
+      headers: {
+        Authorization: `Bearer ${access_token}`,
+      },
+    }
+  ).then((response) => response.json())
+
+  const userInfoSchema = z.object({
+    id: z.string(),
+    email: z.string().email(),
+    name: z.string(),
+    picture: z.string().url(),
+  })
+
+  const userInfo = userInfoSchema.safeParse(googleUserData)
+
+  if (!userInfo.success)
+    return res.status(400).json({ message: 'Invalid google access token.' })
+
+  const { email, id, name, picture } = userInfo.data
+
+  let user = await prismaClient.user.findUnique({
+    where: {
+      google_id: id,
+    },
+  })
+
+  if (!user) {
+    user = await prismaClient.user.create({
+      data: {
+        name,
+        email,
+        avatar_url: picture,
+        google_id: id,
+        subscribe: false,
+      },
+    })
+  }
+
+  const token = jwt.sign(
+    {
+      name: user.name,
+      email: user.email,
+      avatar_url: user.avatar_url,
+      subscribe: user.subscribe,
+    },
+    process.env.JWT_SECRET as string
+  )
+
+  return res.status(201).json({ token })
+}
