6124fe766cf88379a47fa0220f47b585c82a71b3
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2023-01-09 17:44:30 -0300

    create deleteUser controller

---
+++ controllers/user/deleteUser.ts
@@ -0,0 +1,29 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+import { prismaClient } from '../../lib/prisma'
+import { getUserFromCookies } from '../../lib/getUserFromCookies'
+
+export async function deleteUser(req: NextApiRequest, res: NextApiResponse) {
+  const { email } = getUserFromCookies(req)
+
+  try {
+    const user = await prismaClient.user.findMany({
+      where: {
+        email,
+      },
+    })
+
+    if (user.length === 0)
+      return res.status(404).json({ message: 'User not found.' })
+
+    await prismaClient.user.delete({
+      where: {
+        email,
+      },
+    })
+
+    return res.status(204).end()
+  } catch (error) {
+    console.log(error)
+    return res.status(500).json({ message: 'Internal server error.' })
+  }
+}
