66c1c335fb8f41ce248eb8687ba5991384dce125
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2023-01-09 11:41:44 -0300

    remove jwt util

--- utils/jwt.ts
+++
@@ -1,7 +0,0 @@
-import jwt from 'jsonwebtoken'
-
-export const jwtSign = (payload: string | object) =>
-  jwt.sign(payload, process.env.JWT_SECRET as string)
-
-export const jwtVerify = (token: string) =>
-  jwt.verify(token, process.env.JWT_SECRET as string)
