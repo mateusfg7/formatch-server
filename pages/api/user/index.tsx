d8675a824aac34cf9d291345cf21ae4a0f45b5e3
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2023-01-09 11:41:31 -0300

    create user authentication endpoint

---
+++ pages/api/user/index.tsx
@@ -0,0 +1,16 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+
+import { authUser } from '../../../controllers/user/authUser'
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const { method } = req
+
+  if (method === 'GET') authUser(req, res)
+  else {
+    res.setHeader('Allow', ['GET'])
+    res.status(405).end(`Method ${method} Not Allowed`)
+  }
+}
