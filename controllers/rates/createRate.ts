33b7ca7dbb4533eb63731c2c09245244049c3bdf
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2023-01-10 13:18:44 -0300

    feat(controller): add `createRate` controller

---
+++ controllers/rates/createRate.ts
@@ -0,0 +1,65 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+import { z } from 'zod'
+
+import { prismaClient } from '@lib/prisma'
+import { getUserFromCookies } from '@lib/getUserFromCookies'
+
+export async function createRate(req: NextApiRequest, res: NextApiResponse) {
+  const { rate } = req.body
+  const { id } = req.query
+
+  const idSchema = z.string().uuid()
+  const rateSchema = z.number()
+
+  const parsedId = idSchema.safeParse(id)
+  const parsedRate = rateSchema.safeParse(rate)
+
+  if (!parsedId.success) {
+    return res.status(400).json({
+      message: 'Invalid professional ID',
+      error: parsedId.error.issues,
+    })
+  }
+  if (!parsedRate.success) {
+    return res
+      .status(400)
+      .json({ message: 'Invalid rate value', error: parsedRate.error.issues })
+  }
+  if (parsedRate.data < 1 || parsedRate.data > 5) {
+    return res.status(400).json({ message: 'The rate MUST be between 1 and 5' })
+  }
+
+  const { email } = getUserFromCookies(req)
+
+  try {
+    const existingRate = await prismaClient.rate.findMany({
+      where: {
+        AND: [{ user: { email } }, { professional: { id: parsedId.data } }],
+      },
+    })
+
+    if (existingRate.length > 0)
+      return res.status(401).json({ message: 'Professional already rated' })
+
+    const registeredRate = await prismaClient.rate.create({
+      data: {
+        user: {
+          connect: {
+            email,
+          },
+        },
+        professional: {
+          connect: {
+            id: parsedId.data,
+          },
+        },
+        rate_value: parsedRate.data,
+      },
+    })
+
+    return res.status(201).json(registeredRate)
+  } catch (error) {
+    console.log(error)
+    return res.status(500).json({ message: 'Internal server error' })
+  }
+}
