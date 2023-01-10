6102b54e4d0c4251a814a7af06a741ade244682e
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2023-01-10 16:08:18 -0300

    feat(controller): update `findProfessional` controller
    * Use code query param as professional identifier
    * Verify if professional is saved by user
    * Verify if professional is rated by user
    * Calculate average rate
    * Filter response

--- controllers/professionals/findProfessional.ts
+++ controllers/professionals/findProfessional.ts
@@ -2,36 +2,86 @@ import { NextApiRequest, NextApiResponse } from 'next'
 import { z } from 'zod'
 
 import { prismaClient } from '@lib/prisma'
+import { getUserFromCookies } from '@lib/getUserFromCookies'
+import { getArrayAverage } from '@utils/arrayAverage'
 
 export async function findProfessional(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  const { body } = req
+  const { code } = req.query
 
-  const requestBodySchema = z.object({
-    professional_id: z.string(),
-  })
-  const parsedRequestBody = requestBodySchema.safeParse(body)
+  const professionalCodeSchema = z.string()
+  const parsedProfessionalCode = professionalCodeSchema.safeParse(code)
 
-  if (!parsedRequestBody.success)
-    return res.status(405).json({ error: parsedRequestBody.error.issues[0] })
+  if (!parsedProfessionalCode.success)
+    return res.status(400).json({ error: parsedProfessionalCode.error.issues })
 
-  const { professional_id } = parsedRequestBody.data
+  const professionalCode = parsedProfessionalCode.data
+
   try {
     const professional = await prismaClient.professional.findUnique({
       where: {
-        id: professional_id,
+        code: professionalCode,
       },
       include: {
-        services: true,
+        services: {
+          select: {
+            service_name: true,
+          },
+        },
+        Rates: {
+          include: {
+            user: {
+              select: {
+                email: true,
+              },
+            },
+          },
+        },
+        saved_users: {
+          select: {
+            email: true,
+          },
+        },
       },
     })
-    if (professional) {
-      res.status(200).json({ professional })
-    } else {
-      res.status(404).json({ message: 'Professional not found.' })
+
+    if (!professional) {
+      return res.status(404).json({ message: 'Professional not found.' })
     }
+
+    const user = getUserFromCookies(req)
+
+    const isSaved = professional.saved_users.includes({ email: user.email })
+
+    const ratedByUser = professional.Rates.find(
+      (rate) => rate.user.email === user.email
+    )
+    const isRated = ratedByUser != undefined ? true : false
+
+    const ratings = professional.Rates.map((rate) => rate.rate_value)
+    const averageRate = getArrayAverage(ratings)
+
+    const {
+      Rates,
+      saved_users,
+      id,
+      user_owner_id,
+      profile_picture_gcs_path,
+      createdAt,
+      updatedAt,
+      services,
+      ...rest
+    } = professional
+
+    return res.status(200).json({
+      ...rest,
+      isSaved,
+      isRated,
+      averageRate,
+      services: services.map((service) => service.service_name),
+    })
   } catch (err) {
     console.error(err)
     return res
