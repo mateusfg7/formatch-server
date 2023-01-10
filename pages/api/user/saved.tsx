8d0676ffe93b44ebf8c6c8930805b518c37a7d62
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2023-01-09 21:02:09 -0300

    create endpoint to save a professional by a user

---
+++ pages/api/user/saved.tsx
@@ -0,0 +1,19 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+
+import { testDbConnection } from '@lib/testDbConnection'
+import { savedProfessionalByUser } from '@controllers/user/savedProfessionalsByUser'
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const { method } = req
+
+  testDbConnection(res)
+
+  if (method === 'GET') savedProfessionalByUser(req, res)
+  else {
+    res.setHeader('Allow', ['GET'])
+    res.status(405).end(`Method ${method} Not Allowed`)
+  }
+}
