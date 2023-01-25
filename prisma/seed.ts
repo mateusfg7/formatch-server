5f05c3400a67744d9cd9e249cee028e8318a5a07
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2023-01-25 09:14:58 -0300

    feat(prisma): update seed file

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,19 +1,40 @@
 import { PrismaClient } from '@prisma/client'
 
+import articles from '../assets/articles.json'
+import articleWithAd from '../assets/article-with-ad.json'
+
 const prisma = new PrismaClient()
 
 async function main() {
-  const user = await prisma.user.create({
-    data: {
-      name: 'Mateus Felipe',
-      email: 'mateusfelipefg77@gmail.com',
-      avatar_url: 'https://github.com/mateusfg7.png',
-      google_id: '434136468',
-      subscribe: true,
-    },
-  })
+  try {
+    await prisma.user.create({
+      data: {
+        name: 'Mateus Felipe',
+        email: 'mateusfelipefg77@gmail.com',
+        avatar_url: 'https://github.com/mateusfg7.png',
+        google_id: '434136468',
+        subscribe: true,
+      },
+    })
+  } catch (error) {
+    console.log(error)
+  }
 
-  console.log(user)
+  try {
+    await prisma.article.create({
+      data: articleWithAd,
+    })
+  } catch (error) {
+    console.log(error)
+  }
+
+  try {
+    await prisma.article.createMany({
+      data: articles,
+    })
+  } catch (error) {
+    console.log(error)
+  }
 }
 
 main()
