43859b10dee79d8751cf86b822658ff55b2df837
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2023-01-26 13:01:06 -0300

    feat: create article pages

---
+++ pages/article/create.tsx
@@ -0,0 +1,9 @@
+import ArticleForm from '@components/ArticleForm'
+
+export default function CreateArticle() {
+  return (
+    <div className='w-11/12 sm:w-3/4 md:w-2/3 lg:w-1/2 m-auto py-7'>
+      <ArticleForm />
+    </div>
+  )
+}
