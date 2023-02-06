2e8b8f9e16a9302a11c218ff22850ab9413fc16c
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2023-02-06 11:42:45 -0300

    refactor(controller): remove unused interface on `createProfessional`

--- controllers/professionals/createProfessional.ts
+++ controllers/professionals/createProfessional.ts
@@ -165,19 +165,3 @@ export async function createProfessional(
     return res.status(500).json({ error: e, message: 'Internal server error' })
   }
 }
-
-interface AuthenticatedProfessionalData {
-  name: string
-  code: string
-  profile_picture_url: string
-  state_uf: string
-  city: string
-  biography: string
-  services: string[]
-  savedCount: number
-  averageRate: number
-  email: string | null
-  phone: string | null
-  whatsapp: string | null
-  instagram: string | null
-}
