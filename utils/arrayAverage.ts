afa267027ee0864fd89d03838c6714eb05dff03f
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2023-02-09 15:34:49 -0300

    feat(utils): improve `arrayAverage` function
    If was just one rate, return it without get the average value

--- utils/arrayAverage.ts
+++ utils/arrayAverage.ts
@@ -1,6 +1,6 @@
 export function getArrayAverage(listOfNumbers: number[]) {
   if (listOfNumbers.length === 0) return 0
-  if (listOfNumbers.length === 1) return 1
+  if (listOfNumbers.length === 1) return listOfNumbers[0]
 
   const sum = listOfNumbers.reduce((t, n) => t + n)
   const average = sum / listOfNumbers.length
