950ebe110c88df634e87a7b3ab9ed2bb2c7e5c57
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2023-02-09 15:43:04 -0300

    feat(controller): update `savedProfessionalsByUser`
    Exclude some sensible content from response

--- controllers/user/savedProfessionalsByUser.ts
+++ controllers/user/savedProfessionalsByUser.ts
@@ -22,7 +22,15 @@ export async function savedProfessionalByUser(
     if (!savedProfessionals)
       return res.status(404).json({ message: 'User not found.' })
 
-    return res.status(200).json(savedProfessionals.saved_professionals)
+    const savedProfessionalList = savedProfessionals.saved_professionals.map(
+      (professional) => {
+        const { id, user_owner_id, profile_picture_gcs_path, ...rest } =
+          professional
+        return { ...rest }
+      }
+    )
+
+    return res.status(200).json(savedProfessionalList)
   } catch (error) {
     console.log(error)
     return res.status(500).end()
