ba87fa458489d1a5005975390c009fe8a1b6dcb5
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2023-02-15 13:41:46 -0300

    chore(next): add image config to `next.config.js`

--- next.config.js
+++ next.config.js
@@ -2,6 +2,17 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  experimental: {
+    appDir: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
+  },
 }
 
 module.exports = nextConfig
