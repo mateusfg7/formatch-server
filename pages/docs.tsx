743ec1ab842c41ca02c73aecae60e5e41c74de94
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2023-02-20 21:07:40 +0000

    feat: get swagger spec from "/openapi.json"

--- pages/docs.tsx
+++ pages/docs.tsx
@@ -1,41 +1,38 @@
-import { GetStaticProps, InferGetStaticPropsType } from 'next'
-import { createSwaggerSpec } from 'next-swagger-doc'
 import dynamic from 'next/dynamic'
 import { SwaggerUIProps } from 'swagger-ui-react'
 
 import 'swagger-ui-react/swagger-ui.css'
 
 import { Header } from 'components/Header'
+import { useEffect, useState } from 'react'
 
 const SwaggerUI = dynamic<SwaggerUIProps>(import('swagger-ui-react'), {
   ssr: false,
 })
 
-function ApiDoc({ spec }: InferGetStaticPropsType<typeof getStaticProps>) {
+function ApiDoc() {
+  const [spec, setSpec] = useState<object>({})
+
+  async function fetchSpec() {
+    const data = await fetch('/openapi.json').then((response) =>
+      response.json()
+    )
+
+    setSpec(data)
+  }
+
+  useEffect(() => {
+    fetchSpec()
+  }, [])
+
   return (
-    <div className='px-content'>
+    <div>
       <Header />
-      <SwaggerUI spec={spec} />
+      <div className='px-content'>
+        <SwaggerUI spec={spec} />
+      </div>
     </div>
   )
-}
-
-export const getStaticProps: GetStaticProps = async () => {
-  const spec: Record<string, any> = createSwaggerSpec({
-    definition: {
-      openapi: '3.0.0',
-      info: {
-        title: 'Next Swagger API Example',
-        version: '1.0',
-      },
-    },
-  })
-
-  return {
-    props: {
-      spec,
-    },
-  }
 }
 
 export default ApiDoc
