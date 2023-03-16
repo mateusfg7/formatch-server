ffcfb205ca3d6ec98735c0f8b4c7a1128daeb320
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2023-03-16 13:35:37 -0300

    feat(ui/ux): add responsivity on Header component

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,46 +6,41 @@ import { usePathname } from 'next/navigation'
 export function Header() {
   const pathname = usePathname()
 
+  const MenuItem = ({ title, path }: { title: string; path: string }) => {
+    let isActive: boolean
+
+    if (path === '/') {
+      isActive = pathname === path
+      console.log('is home')
+    } else {
+      isActive = pathname?.startsWith(path)
+      console.log('is not home')
+    }
+
+    return (
+      <Link
+        href={path}
+        className={`text-xl rounded-xl p-2 transition duration-500 hover:bg-neutral-100 ${
+          isActive && 'font-bold'
+        }`}
+      >
+        <p className='w-max'>{title}</p>
+      </Link>
+    )
+  }
+
   return (
     <header className='flex flex-col gap-6 pt-7'>
-      <section className='w-full px-content'>
+      <section className='flex justify-center md:justify-start w-full px-content'>
         <div className='w-44'>
           <Image src='/logotipo.svg' width={500} height={500} alt='Formatch' />
         </div>
       </section>
-      <nav className='flex gap-7 w-full px-content py-3 border-b border-black/10'>
-        <Link
-          href='/'
-          className={`text-xl rounded-xl p-2 transition duration-500 hover:bg-neutral-100 ${
-            pathname === '/' && 'font-bold'
-          }`}
-        >
-          Home
-        </Link>
-        <Link
-          href='/articles'
-          className={`text-xl rounded-xl p-2 transition duration-500 hover:bg-neutral-100 ${
-            pathname?.startsWith('/articles') && 'font-bold'
-          }`}
-        >
-          Articles
-        </Link>
-        <Link
-          href='/advertisers'
-          className={`text-xl rounded-xl p-2 transition duration-500 hover:bg-neutral-100 ${
-            pathname?.startsWith('/advertisers') && 'font-bold'
-          }`}
-        >
-          Advertisers
-        </Link>
-        <Link
-          href='/docs'
-          className={`text-xl rounded-xl p-2 transition duration-500 hover:bg-neutral-100 ${
-            pathname?.startsWith('/docs') && 'font-bold'
-          }`}
-        >
-          API Docs
-        </Link>
+      <nav className='flex gap-7 w-full overflow-x-scroll md:overflow-visible px-content py-3 border-b border-black/10'>
+        <MenuItem title='Home' path='/' />
+        <MenuItem title='Articles' path='/articles' />
+        <MenuItem title='Advertisers' path='/advertisers' />
+        <MenuItem title='API Docs' path='/docs' />
       </nav>
     </header>
   )
