63336f7b66e924c01e8402231a042853e44e104d
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2023-03-22 10:39:28 -0300

    feat(api): update `use-case/articles/listArticles`
    Sort articles as a desc order by createdAt

--- use-case/articles/listArticles.ts
+++ use-case/articles/listArticles.ts
@@ -1,20 +1,20 @@
-import { prismaClient } from '@lib/prisma'
+import { prismaClient } from "@lib/prisma";
 
 export async function listArticles() {
   try {
     const articles = await prismaClient.article.findMany({
-      orderBy: { createdAt: 'asc' },
+      orderBy: { createdAt: "desc" },
       include: { AdMeta: true },
-    })
+    });
 
     const parsedArticles = articles.map(({ source, ...rest }) => ({
-      ...(source && { sources: source?.split(',') }),
+      ...(source && { sources: source?.split(",") }),
       ...rest,
-    }))
+    }));
 
-    return parsedArticles
+    return parsedArticles;
   } catch (error) {
-    console.log(error)
-    throw error
+    console.log(error);
+    throw error;
   }
 }
